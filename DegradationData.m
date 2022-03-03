2e51148c63dd35f83b337da10fcd9cfdcb4c99df
Seetaram Maurya <81798420+Seetaram-Maurya@users.noreply.github.com>
2022-03-04 00:31:05 +0530

    Update DegradationData.m

--- DegradationData.m
+++ DegradationData.m
@@ -12,11 +12,11 @@ alpha3=2;
 beta = 0.009;
 
 % define model to generate data
-S1 = alpha1 + exp(t(1,1:180) * beta);
+S1 = alpha1*exp(t(1,1:180) * beta);
 S1=awgn(S1,30,'measured'); 
-S2 = alpha2 + exp(t(1,190:300) * beta);
+S2 = alpha2*exp(t(1,190:300) * beta);
 S2= awgn(S2,30,'measured'); 
-S3 = alpha3 + exp(t(1,310:450) * beta);
+S3 = alpha3*exp(t(1,310:450) * beta);
 S3= awgn(S3,30,'measured'); 
 
 % concatenation 
